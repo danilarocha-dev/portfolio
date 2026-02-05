cc7e570a0f450eb7f6101bd3fb0efb46bde6ae8d
Danila Rocha <danila.rocha.gloria@gmail.com>
2026-02-05 15:36:53 -0300

    feat: implementa alternância de tema claro/escuro

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const botao = document.getElementById('botao-tema');
+const body = document.body;
+
+// Persistência do tema
+const temasalvo = localStorage.getItem('tema');
+temaEscuro(temasalvo === 'escuro');
+
+// Função para alternar entre tema claro e escuro
+function temaEscuro(tipo) {
+  if (tipo == true) {
+    body.classList.add('escuro');
+    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+  } else {
+    body.classList.remove('escuro');
+    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  }
+}
